8bb6c4c6810d69e64cbeb4d2f4a69d03321c2300
Quoiquecesoit <carolina.quesada.vilchez@gmail.com>
2022-10-10 18:38:08 -0400

    Simplified, removed test images, merged home with login pages. Styled. Added new course.

--- settings/functions.js
+++ settings/functions.js
@@ -1,25 +1,54 @@
+function processLogin() {
 
+    let d = new Date;
+    let hour = d.getHours();
+    let minutes = d.getMinutes();
 
-function processLogin()
-{
-let d = new Date;
-let hour = d.getHours();
-let minutes = d.getMinutes();
-
-window.sessionStorage.setItem("loginTime",hour+": "+minutes);
-window.sessionStorage.setItem("agent", navigator.userAgent);
-
-// For testing
-alert("For testing purposes Login Time: "+window.sessionStorage.getItem("loginTime")+"\nAgent: "+window.sessionStorage.getItem("agent"));
-
+    window.sessionStorage.setItem("loginTime", hour + ": " + minutes);
+    window.sessionStorage.setItem("agent", navigator.userAgent);
 
 }
 
-var processLogout = function() {
+let processLogout = function () {
 
-	sessionStorage.clear();
-	alert("Session ended");
+    sessionStorage.clear();
+};
+
+function comparepasswords() {
+
+    let password = document.getElementById("nepassword");
+    let confirmpassword = document.getElementById("neconfirm-password");
+
+    if (password !== confirmpassword) {
+        alert("password confirmation must match the password chosen");
+    }
+
 }
 
+function myFunction() {
+    var ns = document.getElementById("nspassword");
+    var nsc = document.getElementById("nsconfirm-password")
+    var ne = document.getElementById("nepassword");
+    var nec = document.getElementById("neconfirm-password")
 
-
+    if (ns.type === "password") {
+        ns.type = "text";
+    } else {
+        ns.type = "password";
+    }
+    if (nsc.type === "password") {
+        nsc.type = "text";
+    } else {
+        nsc.type = "password";
+    }
+    if (ne.type === "password") {
+        ne.type = "text";
+    } else {
+        ne.type = "password";
+    }
+    if (nec.type === "password") {
+        nec.type = "text";
+    } else {
+        nec.type = "password";
+    }
+}
